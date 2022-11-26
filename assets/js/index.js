9c7788e23b8dc57f062c9e93d0912c988538feb6
wonderboy_xo <109493986+Wonderboyxo@users.noreply.github.com>
2022-11-26 20:56:47 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,114 @@
+const btn1 = document.getElementById("div1")
+const kes1 = document.getElementById("ic1")
+
+function mouseOver1(){
+    kes1.style.color = "rgb(150,201,255)"
+    btn1.style.borderLeftColor = "rgb(150,201,255)"
+    btn1.style.borderLeftStyle = "solid";
+}
+
+function mouseOut1(){
+    kes1.style.color = "rgb(160,171,148)"
+    btn1.style.borderLeftColor = "none";
+    btn1.style.borderLeftStyle = "none";
+    
+}
+
+btn1.addEventListener("mouseover", mouseOver1);
+btn1.addEventListener("mouseout", mouseOut1);
+
+const btn2 = document.getElementById("div2")
+const kes2 = document.getElementById("ic2")
+
+function mouseOver2(){
+    kes2.style.color = "rgb(150,201,255)"
+    btn2.style.borderLeftColor = "rgb(150,201,255)"
+    btn2.style.borderLeftStyle = "solid";
+}
+
+function mouseOut2(){
+    kes2.style.color = "rgb(160,171,148)"
+    btn2.style.borderLeftColor = "none";
+    btn2.style.borderLeftStyle = "none";
+    
+}
+
+btn2.addEventListener("mouseover", mouseOver2);
+btn2.addEventListener("mouseout", mouseOut2);
+
+const btn3 = document.getElementById("div3")
+const kes3 = document.getElementById("ic3")
+
+function mouseOver3(){
+    kes3.style.color = "rgb(150,201,255)"
+    btn3.style.borderLeftColor = "rgb(150,201,255)"
+    btn3.style.borderLeftStyle = "solid";
+}
+
+function mouseOut3(){
+    kes3.style.color = "rgb(160,171,148)"
+    btn3.style.borderLeftColor = "none";
+    btn3.style.borderLeftStyle = "none";
+    
+}
+
+btn3.addEventListener("mouseover", mouseOver3);
+btn3.addEventListener("mouseout", mouseOut3);
+
+const btn4 = document.getElementById("div4")
+const kes4 = document.getElementById("ic4")
+
+function mouseOver4(){
+    kes4.style.color = "rgb(150,201,255)"
+    btn4.style.borderLeftColor = "rgb(150,201,255)"
+    btn4.style.borderLeftStyle = "solid";
+}
+
+function mouseOut4(){
+    kes4.style.color = "rgb(160,171,148)"
+    btn4.style.borderLeftColor = "none";
+    btn4.style.borderLeftStyle = "none";
+    
+}
+
+btn4.addEventListener("mouseover", mouseOver4);
+btn4.addEventListener("mouseout", mouseOut4);
+
+const btn5 = document.getElementById("div5")
+const kes5 = document.getElementById("ic5")
+
+function mouseOver5(){
+    kes5.style.color = "rgb(150,201,255)"
+    btn5.style.borderLeftColor = "rgb(150,201,255)"
+    btn5.style.borderLeftStyle = "solid";
+}
+
+function mouseOut5(){
+    kes5.style.color = "rgb(160,171,148)"
+    btn5.style.borderLeftColor = "none";
+    btn5.style.borderLeftStyle = "none";
+    
+}
+
+btn5.addEventListener("mouseover", mouseOver5);
+btn5.addEventListener("mouseout", mouseOut5);
+
+const btn6 = document.getElementById("div6")
+const kes6 = document.getElementById("ic6")
+
+function mouseOver6(){
+    kes6.style.color = "rgb(150,201,255)"
+    btn6.style.borderLeftColor = "rgb(150,201,255)"
+    btn6.style.borderLeftStyle = "solid";
+}
+
+function mouseOut6(){
+    kes6.style.color = "rgb(160,171,148)"
+    btn6.style.borderLeftColor = "none";
+    btn6.style.borderLeftStyle = "none";
+    
+}
+
+btn6.addEventListener("mouseover", mouseOver6);
+btn6.addEventListener("mouseout", mouseOut6);
+
